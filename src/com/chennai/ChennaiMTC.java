6d2a943546c9c6bcbd130194a7a861af0c2cf1d7
shivamma suresh <suresh6515@gmail.com>
2019-04-02 21:54:28 +0530

    mtc done

---
+++ src/com/chennai/ChennaiMTC.java
@@ -0,0 +1,36 @@
+package com.chennai;
+
+public class ChennaiMTC {
+
+	private int busNo;
+	
+	private String busRoute;
+	
+	private String busDriver;
+
+	public int getBusNo() {
+		return busNo;
+	}
+
+	public void setBusNo(int busNo) {
+		this.busNo = busNo;
+	}
+
+	public String getBusRoute() {
+		return busRoute;
+	}
+
+	public void setBusRoute(String busRoute) {
+		this.busRoute = busRoute;
+	}
+
+	public String getBusDriver() {
+		return busDriver;
+	}
+
+	public void setBusDriver(String busDriver) {
+		this.busDriver = busDriver;
+	}
+	
+	
+}
